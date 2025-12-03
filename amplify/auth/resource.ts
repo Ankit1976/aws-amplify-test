75020b52b6be8505987f08f082997dc6b99bf3d4
Ankit <gamerankit197@gmail.com>
2025-12-03 23:35:11 +0530

    updated auth resource

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,9 +1,9 @@
 import { referenceAuth } from '@aws-amplify/backend';
 
 export const auth = referenceAuth({
-  userPoolId: 'ap-south-1_2PIWk3X1h',
-  identityPoolId: 'ap-south-1:d6b690cb-d868-4e63-b5e8-88728b276577',
+  userPoolId: 'ap-south-1_aHG9IodaE',
+  identityPoolId: 'ap-south-1:42948012-e1f1-450d-a72a-026b5eebe567',
   authRoleArn: 'arn:aws:iam::705473094944:role/service-role/shaurrik-test-identity-pool-to-user-pool-role',
   unauthRoleArn: 'arn:aws:iam::705473094944:role/service-role/amplify-unauthenticated-role',
-  userPoolClientId: '4aaeohsio5mra9g1rcj8asevdl',
+  userPoolClientId: '77o32986hjhdlc2j8ti9ekr8fb',
 });
